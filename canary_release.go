c97183784a5aa680fe1af0f49e989bc6eeb290b7
litesun <sunyi@apache.org>
2022-11-21 20:37:57 +0800

    remove useless comment

--- canary_release.go
+++ canary_release.go
@@ -51,7 +51,7 @@ type CanaryReleaseRule struct {
 type CanaryReleaseSpec struct {
 	// Name specify the name of canary release
 	Name string `json:"name"`
-	// State specify the state of the canary release.Can be "pause", "in_progress", "finish"
+	// State specify the state of the canary release.
 	// Optional values can be:
 	// * CanaryReleaseStatePause:  the object state is pause.
 	// * CanaryReleaseStateInProgress:  the object state is in_progress.
