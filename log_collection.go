8e3aa51a43a0e49ae933e0f80ea713d5d465d16d
Baoyuan <baoyuan.top@gmail.com>
2022-11-24 18:25:37 +0800

    feat: add log collection operations (#14)

---
+++ log_collection.go
@@ -0,0 +1,157 @@
+// Copyright 2022 API7.ai, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloud
+
+import (
+	"context"
+	"path"
+)
+
+// LogCollectionType is the type of log collection
+type LogCollectionType string
+
+var (
+	// HTTPLogCollection means http log collection
+	HTTPLogCollection LogCollectionType = "http-logger"
+	// KakfaLogCollection means kafka log collection
+	KakfaLogCollection LogCollectionType = "kafka-logger"
+)
+
+// LogCollection is the abstraction of log storage
+type LogCollection struct {
+	// ID is the unique identify to mark an object.
+	ID ID `json:"id"`
+	// Name is the name of log collection
+	Name string `json:"name"`
+	// Description is the description of log collection
+	Description string `json:"description"`
+	// Type is the type of log collection
+	Type LogCollectionType `json:"type"`
+	// Spec is the specification of log collection
+	Spec interface{} `json:"spec" gorm:"serializer:json"`
+}
+
+// LogCollectionInterface is the interface of the LogCollection
+type LogCollectionInterface interface {
+	// CreateLogCollection creates an API7 Cloud Log Collection in the specified control plane.
+	// The given `lc` parameter should specify the desired LogCollection specification.
+	// Users need to specify the ControlPlane in the `opts`.
+	// The returned LogCollection will contain the same LogCollection specification plus some
+	// management fields and default values.
+	CreateLogCollection(ctx context.Context, lc *LogCollection, opts *ResourceCreateOptions) (*LogCollection, error)
+	// UpdateLogCollection updates an existing API7 Cloud Log Collection in the specified control plane.
+	// The given `lc` parameter should specify the desired LogCollection specification.
+	// Users need to specify the ControlPlane in the `opts`.
+	// The returned LogCollection will contain the same LogCollection specification plus some
+	// management fields and default values.
+	UpdateLogCollection(ctx context.Context, lc *LogCollection, opts *ResourceUpdateOptions) (*LogCollection, error)
+	// DeleteLogCollection deletes an existing API7 Cloud Log Collection in the specified control plane.
+	// The given `lcID` parameter should specify the LogCollection that you want to delete.
+	// Users need to specify the ControlPlane in the `opts`.
+	DeleteLogCollection(ctx context.Context, lcID ID, opts *ResourceDeleteOptions) error
+	// GetLogCollection gets an existing API7 Cloud Log Collection in the specified control plane.
+	// The given `lcID` parameter should specify the LogCollection that you want to get.
+	// Users need to specify the ControlPlane in the `opts`.
+	GetLogCollection(ctx context.Context, lcID ID, opts *ResourceGetOptions) (*LogCollection, error)
+	// ListLogCollections returns an iterator for listing Log Collections in the specified control plane with the
+	// given list conditions.
+	// Users need to specify the ControlPlane, Paging conditions in the `opts`.
+	ListLogCollections(ctx context.Context, opts *ResourceListOptions) (LogCollectionIterator, error)
+}
+
+// LogCollectionIterator is an iterator for listing Log Collections.
+type LogCollectionIterator interface {
+	// Next returns the next Log Collection according to the filter conditions.
+	Next() (*LogCollection, error)
+}
+
+type logCollectionIterator struct {
+	iter listIterator
+}
+
+type logCollectionImpl struct {
+	client httpClient
+}
+
+func (iter *logCollectionIterator) Next() (*LogCollection, error) {
+	app, err := iter.iter.Next()
+	if err != nil {
+		return nil, err
+	}
+	if app == nil {
+		return nil, nil
+	}
+	return app.(*LogCollection), nil
+}
+
+func newLogCollection(cli httpClient) LogCollectionInterface {
+	return &logCollectionImpl{
+		client: cli,
+	}
+}
+
+func (impl *logCollectionImpl) CreateLogCollection(ctx context.Context, lc *LogCollection, opts *ResourceCreateOptions) (*LogCollection, error) {
+	var createdLogCollection LogCollection
+
+	cpID := opts.ControlPlane.ID
+	uri := path.Join(_apiPathPrefix, "controlplanes", cpID.String(), "log_collections")
+	err := impl.client.sendPostRequest(ctx, uri, "", lc, jsonPayloadDecodeFactory(createdLogCollection))
+	if err != nil {
+		return nil, err
+	}
+	return &createdLogCollection, nil
+}
+
+func (impl *logCollectionImpl) UpdateLogCollection(ctx context.Context, lc *LogCollection, opts *ResourceUpdateOptions) (*LogCollection, error) {
+	var createdLogCollection LogCollection
+
+	cpID := opts.ControlPlane.ID
+	uri := path.Join(_apiPathPrefix, "controlplanes", cpID.String(), "log_collections", lc.ID.String())
+	err := impl.client.sendPutRequest(ctx, uri, "", lc, jsonPayloadDecodeFactory(createdLogCollection))
+	if err != nil {
+		return nil, err
+	}
+	return &createdLogCollection, nil
+}
+
+func (impl *logCollectionImpl) DeleteLogCollection(ctx context.Context, lcID ID, opts *ResourceDeleteOptions) error {
+	cpID := opts.ControlPlane.ID
+	uri := path.Join(_apiPathPrefix, "controlplanes", cpID.String(), "log_collections", lcID.String())
+	return impl.client.sendDeleteRequest(ctx, uri, "", nil)
+}
+
+func (impl *logCollectionImpl) GetLogCollection(ctx context.Context, lcID ID, opts *ResourceGetOptions) (*LogCollection, error) {
+	var logcollection LogCollection
+
+	cpID := opts.ControlPlane.ID
+	uri := path.Join(_apiPathPrefix, "controlplanes", cpID.String(), "log_collections", lcID.String())
+	err := impl.client.sendGetRequest(ctx, uri, "", jsonPayloadDecodeFactory(&logcollection))
+	if err != nil {
+		return nil, err
+	}
+	return &logcollection, nil
+}
+
+func (impl *logCollectionImpl) ListLogCollections(ctx context.Context, opts *ResourceListOptions) (LogCollectionIterator, error) {
+	iter := listIterator{
+		ctx:      ctx,
+		resource: "logcollection",
+		client:   impl.client,
+		path:     path.Join(_apiPathPrefix, "controlplanes", opts.ControlPlane.ID.String(), "log_collections"),
+		paging:   mergePagination(opts.Pagination),
+	}
+
+	return &logCollectionIterator{iter: iter}, nil
+}
---
+++ log_collection_test.go
@@ -0,0 +1,280 @@
+// Copyright 2022 API7.ai, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloud
+
+import (
+	"context"
+	"errors"
+	"path"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateLogCollection(t *testing.T) {
+	t.Parallel()
+
+	testCase := []struct {
+		name                 string
+		pendingLogCollection *LogCollection
+		expectedError        string
+		mockFunc             func(t *testing.T) httpClient
+	}{
+		{
+			name: "create successfully",
+			pendingLogCollection: &LogCollection{
+				Name: "test log collection",
+			},
+			mockFunc: func(t *testing.T) httpClient {
+				ctrl := gomock.NewController(t)
+				cli := NewMockhttpClient(ctrl)
+				cli.EXPECT().sendPostRequest(gomock.Any(), path.Join(_apiPathPrefix, "/controlplanes/1/log_collections"), "", gomock.Any(), gomock.Any()).Return(nil)
+				return cli
+			},
+			expectedError: "",
+		},
+		{
+			name: "mock error",
+			pendingLogCollection: &LogCollection{
+				Name: "test log collection",
+			},
+			mockFunc: func(t *testing.T) httpClient {
+				ctrl := gomock.NewController(t)
+				cli := NewMockhttpClient(ctrl)
+				cli.EXPECT().sendPostRequest(gomock.Any(), path.Join(_apiPathPrefix, "/controlplanes/1/log_collections"), "", gomock.Any(), gomock.Any()).Return(errors.New("mock error"))
+				return cli
+			},
+			expectedError: "mock error",
+		},
+	}
+
+	for _, tc := range testCase {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			cli := tc.mockFunc(t)
+			_, err := newLogCollection(cli).CreateLogCollection(context.Background(), tc.pendingLogCollection, &ResourceCreateOptions{
+				ControlPlane: &ControlPlane{
+					ID: 1,
+				},
+			})
+			if tc.expectedError == "" {
+				assert.Nil(t, err, "check log collection create error")
+			} else {
+				assert.Contains(t, err.Error(), tc.expectedError, "check the error details")
+			}
+		})
+	}
+}
+
+func TestDeleteLogCollection(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name          string
+		expectedError string
+		mockFunc      func(t *testing.T) httpClient
+	}{
+		{
+			name: "delete successfully",
+			mockFunc: func(t *testing.T) httpClient {
+				ctrl := gomock.NewController(t)
+				cli := NewMockhttpClient(ctrl)
+				cli.EXPECT().sendDeleteRequest(gomock.Any(), path.Join(_apiPathPrefix, "/controlplanes/1/log_collections/2"), "", nil).Return(nil)
+				return cli
+			},
+			expectedError: "",
+		},
+		{
+			name: "mock error",
+			mockFunc: func(t *testing.T) httpClient {
+				ctrl := gomock.NewController(t)
+				cli := NewMockhttpClient(ctrl)
+				cli.EXPECT().sendDeleteRequest(gomock.Any(), path.Join(_apiPathPrefix, "/controlplanes/1/log_collections/2"), "", nil).Return(errors.New("mock error"))
+				return cli
+			},
+			expectedError: "mock error",
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			cli := tc.mockFunc(t)
+			err := newLogCollection(cli).DeleteLogCollection(context.Background(), 2, &ResourceDeleteOptions{
+				ControlPlane: &ControlPlane{
+					ID: 1,
+				},
+			})
+			if tc.expectedError == "" {
+				assert.Nil(t, err, "check delete log collection error")
+			} else {
+				assert.Contains(t, err.Error(), tc.expectedError, "check the error details")
+			}
+		})
+	}
+}
+
+func TestGetLogCollection(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name          string
+		expectedError string
+		mockFunc      func(t *testing.T) httpClient
+	}{
+		{
+			name: "get successfully",
+			mockFunc: func(t *testing.T) httpClient {
+				ctrl := gomock.NewController(t)
+				cli := NewMockhttpClient(ctrl)
+				cli.EXPECT().sendGetRequest(gomock.Any(), path.Join(_apiPathPrefix, "/controlplanes/1/log_collections/2"), "", gomock.Any()).Return(nil)
+				return cli
+
+			},
+			expectedError: "",
+		},
+		{
+			name: "mock error",
+			mockFunc: func(t *testing.T) httpClient {
+				ctrl := gomock.NewController(t)
+				cli := NewMockhttpClient(ctrl)
+				cli.EXPECT().sendGetRequest(gomock.Any(), path.Join(_apiPathPrefix, "/controlplanes/1/log_collections/2"), "", gomock.Any()).Return(errors.New("mock error"))
+				return cli
+			},
+			expectedError: "mock error",
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			cli := tc.mockFunc(t)
+			// ignore the application check since currently we don't mock it, and the app is always a zero value.
+			_, err := newLogCollection(cli).GetLogCollection(context.Background(), 2, &ResourceGetOptions{
+				ControlPlane: &ControlPlane{
+					ID: 1,
+				},
+			})
+			if tc.expectedError == "" {
+				assert.Nil(t, err, "check get log collection detail error")
+			} else {
+				assert.Contains(t, err.Error(), tc.expectedError, "check the error details")
+			}
+		})
+	}
+}
+
+func TestListLogCollection(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name     string
+		iterator *logCollectionIterator
+	}{
+		{
+			name: "create iterator successfully",
+			iterator: &logCollectionIterator{
+				iter: listIterator{
+					resource: "logcollection",
+					path:     "/api/v1/controlplanes/1/log_collections",
+					paging: Pagination{
+						Page:     2,
+						PageSize: 14,
+					},
+					eof: false,
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			raw, err := newLogCollection(nil).ListLogCollections(context.Background(), &ResourceListOptions{
+				ControlPlane: &ControlPlane{
+					ID: 1,
+				},
+				Pagination: &Pagination{
+					Page:     2,
+					PageSize: 14,
+				},
+			})
+			assert.Nil(t, err, "check list log collection error")
+			iter := raw.(*logCollectionIterator)
+			assert.Equal(t, tc.iterator.iter.resource, iter.iter.resource, "check resource")
+			assert.Equal(t, tc.iterator.iter.path, iter.iter.path, "check path")
+			assert.Equal(t, tc.iterator.iter.paging.Page, iter.iter.paging.Page, "check page")
+			assert.Equal(t, tc.iterator.iter.paging.PageSize, iter.iter.paging.PageSize, "check page size")
+		})
+	}
+}
+
+func TestUpdateLogCollection(t *testing.T) {
+	t.Parallel()
+
+	testCase := []struct {
+		name                 string
+		pendingLogCollection *LogCollection
+		expectedError        string
+		mockFunc             func(t *testing.T) httpClient
+	}{
+		{
+			name: "update successfully",
+			pendingLogCollection: &LogCollection{
+				Name: "test log collection",
+				ID:   2,
+			},
+			mockFunc: func(t *testing.T) httpClient {
+				ctrl := gomock.NewController(t)
+				cli := NewMockhttpClient(ctrl)
+				cli.EXPECT().sendPutRequest(gomock.Any(), path.Join(_apiPathPrefix, "/controlplanes/1/log_collections/2"), "", gomock.Any(), gomock.Any()).Return(nil)
+				return cli
+			},
+			expectedError: "",
+		},
+		{
+			name: "mock error",
+			pendingLogCollection: &LogCollection{
+				Name: "test log collection",
+				ID:   2,
+			},
+			mockFunc: func(t *testing.T) httpClient {
+				ctrl := gomock.NewController(t)
+				cli := NewMockhttpClient(ctrl)
+				cli.EXPECT().sendPutRequest(gomock.Any(), path.Join(_apiPathPrefix, "/controlplanes/1/log_collections/2"), "", gomock.Any(), gomock.Any()).Return(errors.New("mock error"))
+				return cli
+			},
+			expectedError: "mock error",
+		},
+	}
+
+	for _, tc := range testCase {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			cli := tc.mockFunc(t)
+			_, err := newLogCollection(cli).UpdateLogCollection(context.Background(), tc.pendingLogCollection, &ResourceUpdateOptions{
+				ControlPlane: &ControlPlane{
+					ID: 1,
+				},
+			})
+			if tc.expectedError == "" {
+				assert.Nil(t, err, "check log collection create error")
+			} else {
+				assert.Contains(t, err.Error(), tc.expectedError, "check the error details")
+			}
+		})
+	}
+}
